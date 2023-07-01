4b81652f58efd0099b0336b7d0f020f9d349ce9d
liusize <895898088@qq.com>
2023-07-01 18:44:03 +0800

    :art: id自动转化为number

--- packages/server/src/script/dto/update-script.dto.ts
+++ packages/server/src/script/dto/update-script.dto.ts
@@ -1,8 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsString } from 'class-validator';
 import { CreateScriptDto } from './creat-script.dto';
 
 export class UpdateScriptDto extends PartialType(CreateScriptDto) {
+  @Transform(({ value }) => parseInt(value))
   id?: number;
   @IsString()
   @IsNotEmpty()
